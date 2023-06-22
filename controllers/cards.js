94d5ea109528c954a615fea351da65496cc49e6a
Mikhail Krishtopa <m.a.krishtopa@yandex.ru>
2023-06-22 13:22:36 +0400

    fix: remake toggleCardLike

--- controllers/cards.js
+++ controllers/cards.js
@@ -55,10 +55,10 @@ const removeCardById = (req, res) => {
     });
 };
 
-const putCardLike = (req, res) => {
+const toggleCardLike = (req, res, action) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
-    { $addToSet: { likes: req.user._id } },
+    action,
     { new: true },
   )
     .orFail(new Error('CardNotFound'))
@@ -80,29 +80,12 @@ const putCardLike = (req, res) => {
     });
 };
 
+const putCardLike = (req, res) => {
+  toggleCardLike(req, res, { $addToSet: { likes: req.user._id } });
+};
+
 const removeCardLike = (req, res) => {
-  Card.findByIdAndUpdate(
-    req.params.cardId,
-    { $pull: { likes: req.user._id } },
-    { new: true },
-  )
-    .orFail(new Error('CardNotFound'))
-    .then((newCard) => res.status(httpConstants.HTTP_STATUS_OK).send(newCard))
-    .catch((err) => {
-      if (err.message === 'CardNotFound') {
-        return res.status(httpConstants.HTTP_STATUS_NOT_FOUND).send({
-          message: `${statusCodes[httpConstants.HTTP_STATUS_NOT_FOUND]}`,
-        });
-      }
-      if (err instanceof mongoose.Error.CastError) {
-        return res.status(httpConstants.HTTP_STATUS_BAD_REQUEST).send({
-          message: `${statusCodes[httpConstants.HTTP_STATUS_BAD_REQUEST]}, Invalid ID`,
-        });
-      }
-      return res.status(httpConstants.HTTP_STATUS_INTERNAL_SERVER_ERROR).send({
-        message: `${statusCodes[httpConstants.HTTP_STATUS_INTERNAL_SERVER_ERROR]}`,
-      });
-    });
+  toggleCardLike(req, res, { $pull: { likes: req.user._id } });
 };
 
 module.exports = {
