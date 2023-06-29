7f8dfd70297ef2beca20ed294f4d08a5de5cb7a4
Mikhail Krishtopa <m.a.krishtopa@yandex.ru>
2023-06-29 13:00:58 +0400

    fix: remake auth middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,19 +1,20 @@
 /* eslint-disable consistent-return */
 const { verifyToken } = require('../utils/jwt');
+const AuthorizationError = require('../errors/AuthorizationError');
 
 const auth = (req, res, next) => {
   const { authorization } = req.headers;
 
-  return verifyToken(authorization)
-    .then((check) => {
-      req.user = { _id: check.id };
-      // eslint-disable-next-line no-console
-      console.log('Запрос от юзера с ID:', req.user._id);
-      next();
-    })
-    .catch((err) => {
-      next(err);
-    });
+  const check = verifyToken(authorization);
+
+  if (!check) {
+    return next(new AuthorizationError('Authorization required'));
+  }
+
+  req.user = { _id: check.id };
+  // eslint-disable-next-line no-console
+  console.log('Запрос от юзера с ID:', req.user._id);
+  next();
 };
 
 module.exports = { auth };
--- utils/jwt.js
+++ utils/jwt.js
@@ -1,6 +1,4 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/user');
-const AuthorizationError = require('../errors/AuthorizationError');
 
 const JWT_SECRET = 'secret-code';
 
@@ -8,11 +6,10 @@ const generateToken = (id) => jwt.sign({ id }, JWT_SECRET, { expiresIn: '7d' });
 
 const verifyToken = (token) => jwt.verify(token, JWT_SECRET, (err, encoded) => {
   if (err) {
-    return Promise.reject(new AuthorizationError('Authorization required'));
+    return false;
   }
-
-  return User.findOne({ _id: encoded.id })
-    .then((user) => user || new AuthorizationError('Authorization required'));
+  console.log(encoded);
+  return encoded;
 });
 
 module.exports = {
